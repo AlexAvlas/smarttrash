747dae286a46aa4dbf4bd434bcd50713bfaddaca
rajeevbrahma <rajeevbrahma@hotmail.com>
2016-08-04 03:18:13 +0530

    updated

--- Trashcan.py
+++ Trashcan.py
@@ -20,8 +20,8 @@ LOG_FILENAME = 'Trashcanlogs.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,format='%(asctime)s, %(levelname)s, %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 
-account_sid = "AC161d5213dce9632db6d2b6febdad21eb" 
-auth_token  = "9ee4b0327f1e3d09b7a8928bb602ac9b"
+account_sid = ""  # Enter your account sid 
+auth_token  = ""  # Enter your auth token
 
 twilioClient = TwilioRestClient(account_sid, auth_token)
 
@@ -57,18 +57,34 @@ Description		:	Function calculates the amount of waste in the trashcan and send 
 					and sends an alert message when trash can reaches the threshold
 Parameters 		:	-
 ****************************************************************************************'''
+currentCriticalLevelFlag = False
+criticalLevelChangeOverFlag = False
+distanceRecordedTime = 0
+criticalLevelReachedTime = 0
+currentDistance = 0
+notificationSentTime = 0
+
+LOOP_SAMPLING_TIME = 2
+CRITICAL_DISTANCE = 50
+NOTIFICATION_TIME_DELAY = 15
+
+
 
 def distanceMeasurement():
 	try:
-		global client,deviceType
+		global client,deviceType,LOOP_SAMPLING_TIME,NOTIFICATION_TIME_DELAY,CRITICAL_DISTANCE,currentCriticalLevelFlag,criticalLevelChangeOverFlag
 		l_prev_distance = 0
-		previousTime = 0
 		ultrasonicSensorInit()
+
+		deviceId = "TRASHCANAPP_001"
+		deviceType = "IOTIFY_APP"
+		messageBody = "Trashcan at xyz filled please come to pickup"
+			
 			
 		while 1:
 
 			if GPIO.input(LIDCOVER) == 0:
-				time.sleep(2)		
+				time.sleep(LOOP_SAMPLING_TIME)		
 				GPIO.output(TRIG, True)
 				time.sleep(0.00001)
 				GPIO.output(TRIG, False)
@@ -84,55 +100,67 @@ def distanceMeasurement():
 				l_distance = pulse_duration * 17150
 
 				l_distance = round(l_distance, 2)
-				if(l_prev_distance != l_distance):
-					GPIO.output(alarmOut,False)
-					l_prev_distance = l_distance
-					previousTime = 0
-					message = {"ID":1,"distance":l_distance}
-					
-					deviceId = "TRASHCANAPP_001"
-					deviceType = "IOTIFY_APP"
-					
-					try:
-						# publishing the message to the Device called APP
-						pubReturn = client.publishEvent(deviceType, deviceId, "status", "json", message)
-						if pubReturn ==True:
-							logging.info("The message successfully sent")
-					except Exception  as e:
-							logging.info("The sent message Failed")
-							logging.error("The publishEvent exception httpcode :%s,message:%s,response:%s"(e.httpcode,e.message,e.response))
-				if l_distance <50:
-					GPIO.output(alarmOut,True)
-					presentTime = datetime.datetime.now() 
-					 
-					messageBody = "Trashcan at 'area name' filled please come to pickup"
-					if (previousTime!=0):
-						diff = presentTime - previousTime 
 				
-						day  = diff.days
-						hour = (day*24 + diff.seconds/3600)
-						diff_minutes = (diff.days *24*60)+(diff.seconds/60)
-						if diff_minutes >= 15:		
-						  
-							try:
-								# twilio message sent option
-								message = twilioClient.messages.create(body=messageBody,to="+919738300498",from_="+12512724152")	
-								# storing the time for the 15min interval purpose
-								previousTime = datetime.datetime.now()
-							except TwilioRestException as e:
-								previousTime = datetime.datetime.now()
-								print e	
-					elif(previousTime == 0):
-						try:
-							message = twilioClient.messages.create(body=messageBody,to="+919738300498",from_="+12512724152")	
-							previousTime = datetime.datetime.now()
-						except TwilioRestException as e:
-							previousTime = datetime.datetime.now()
-							print e	
-					else:
-						pass	
+				currentDistance = l_distance
+				distanceRecordedTime = datetime.datetime.now()
 
+				message = {"ID":1,"distance":l_distance}
+				
+				try:
+					# publishing the message to the Device called APP
+					pubReturn = client.publishEvent(deviceType, deviceId, "status", "json", message)
+					if pubReturn ==True:
+						logging.info("The message successfully sent")
+				except Exception  as e:
+						logging.info("The sent message Failed")
+						logging.error("The publishEvent exception httpcode :%s,message:%s,response:%s"(e.httpcode,e.message,e.response))
+				
+
+				if currentDistance < CRITICAL_DISTANCE:#6.1
+					GPIO.output(alarmOut,True)
+					if currentCriticalLevelFlag == False:#6.1.1
+						currentCriticalLevelFlag = True#6.1.1.2
+						criticalLevelChangeOverFlag = True#6.1.1.3
+						criticalLevelReachedTime = datetime.datetime.now()
+					else:
+						criticalLevelChangeOverFlag = False
+
+				else:
+					GPIO.output(alarmOut,False)					
+					currentCriticalLevelFlag = False
+					criticalLevelChangeOverFlag = False
+
+
+				#This means that in this measurement loop , the changeover has happend 
+				if criticalLevelChangeOverFlag == True:
+					try:
+						message = twilioClient.messages.create(body=messageBody,to="+919738300498",from_="+12512724152")	
+					except TwilioRestException as e:
+						logging.error("The exception in twilio %s,%s"(e,type(e)))	
+					
+					notificationSentTime = datetime.datetime.now()
+				
+
+				# This means that in this measurement loop the level stays at the critical level
+				
+				elif (currentCriticalLevelFlag == True):
+					#calculate timedifference
+					diff = distanceRecordedTime - notificationSentTime 
 	
+					day  = diff.days
+					hour = (day*24 + diff.seconds/3600)
+					diff_minutes = (diff.days *24*60)+(diff.seconds/60)			
+
+					if diff_minutes > NOTIFICATION_TIME_DELAY:
+						try:								
+							message = twilioClient.messages.create(body=messageBody,to="+919738300498",from_="+12512724152")	
+						except TwilioRestException as e:
+							logging.error("The exception in twilio %s,%s"(e,type(e)))	
+						
+
+						notificationSentTime = datetime.datetime.now()
+			
+						
 	except KeyboardInterrupt: 
 		GPIO.cleanup()
 	except Exception as e:
@@ -148,11 +176,17 @@ Parameters 		:	-
 
 def init():
 	global client,deviceType
-	organization = "c25kx4" #Your organization ID
+	# Enter your organizations ID
+	organization = "" #Your organization ID
+	
 	appId = "TRASHCAN_001"   # The Device you've created and wants to connect with
 	authMethod = "apikey" #Method of authentication (the only value currently supported is apikey)
-	authKey = "a-c25kx4-8jlsytrdfu" #API key (required if auth-method is apikey).
-	authToken = "mK&L+ymD*pI5tzKv0s"#API key token (required if auth-method is apikey).
+	
+	# Enter the autkey and authToken(These you will when you generate the api keys)
+	
+	authKey = "" #API key (required if auth-method is apikey).
+	authToken = ""#API key token (required if auth-method is apikey).
+	
 	deviceType = "iotify_trashcan" # The Type of the device created in your organization 
 	deviceId = "TRASHCAN_001" # The Device you've created and wants to connect with                                                                                                       
 	try:
@@ -166,4 +200,4 @@ def init():
 
 if __name__ == '__main__':
 	init()
-	distanceMeasurement()		
+	distanceMeasurement()		 
